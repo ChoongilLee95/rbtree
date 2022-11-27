66b0763dca4976ce0a66689f35fe0f0c7e0c7789
ChoongilLee <choongil.lee95@gmail.com>
2022-11-28 06:01:26 +0900

    done

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,11 +1,11 @@
 #include "rbtree.h"
-
 #include <stdlib.h>
 
 rbtree *new_rbtree(void) {
     rbtree* p = (rbtree*)calloc(1, sizeof(rbtree));
     p->nil = (node_t*)calloc(1, sizeof(node_t));
     p->root = p->nil;
+    p->nil->color = RBTREE_BLACK; 
     // TODO: initialize struct if needed
     return p;
 }
@@ -23,7 +23,7 @@ static void left_rotate(rbtree* t,node_t* ex_parent)
     node_t* ex_son = ex_parent->right;
     ex_parent->right = ex_son->left;
     if (ex_son->left != t->nil)
-        (ex_son->right)->parent = ex_parent;
+        (ex_son->left)->parent = ex_parent; //에러
     ex_son->parent = ex_parent->parent;
     if (ex_parent->parent == t->nil)
     {
@@ -44,7 +44,7 @@ static void right_rotate(rbtree* t,node_t* ex_parent)
     node_t* ex_son = ex_parent->left;
     ex_parent->left = ex_son->right;
     if (ex_son->right != t->nil)
-        (ex_son->left)->parent = ex_parent;
+        (ex_son->right)->parent = ex_parent;
     ex_son->parent = ex_parent->parent;
     if (ex_parent->parent == t->nil)
     {
@@ -151,9 +151,9 @@ node_t* rbtree_insert(rbtree* t, const key_t inserting_key)
     {
         inserting_parent = inserting_point;
         if (inserting_point->key > inserting_key)
-            inserting_point = inserting_point->right;
+            inserting_point = inserting_point->left;
         // 이 경우에 key 값이 같은 node를 만나는 경우도 포함됨을 의식하자
-        else inserting_point = inserting_point->left;
+        else inserting_point = inserting_point->right;
     }
     
     // inserting의 관점에서 부모를 설정한다.
@@ -179,14 +179,14 @@ node_t* rbtree_find(const rbtree* T, const key_t target_key)
     node_t* finder = T->root;
     while (finder != T->nil)
     {
-        if (finder->key != target_key)
+        if (finder->key == target_key)
             return finder;
         if (finder->key > target_key)
             finder = finder->left;
         else
             finder = finder->right;
     }
-    return finder;
+    return NULL;
 }
 
 // 트리 전체의 최솟값의 key의 포인터를 리턴한다.
@@ -204,7 +204,7 @@ node_t* rbtree_min(const rbtree* T)
 node_t* rbtree_max(const rbtree* T)
 {
     node_t* ans = T->root;
-    while (ans != T->nil)
+    while (ans->right != T->nil)
     {
         ans = ans->right;
     }
@@ -276,7 +276,7 @@ static void del_fixup(rbtree* t, node_t* gray)
                 bro->color = RBTREE_RED;
                 bro->left->color = RBTREE_BLACK;
                 right_rotate(t, bro);
-                bro = bro->parent;
+                bro = gray->parent->right;
             }
             // case 2-2-4 : 종료
             // 조건 : gray -> color == black, bro->color == black, bro->right->color == red
@@ -287,6 +287,52 @@ static void del_fixup(rbtree* t, node_t* gray)
             gray->parent->color = RBTREE_BLACK;
             bro->right->color = RBTREE_BLACK;
             left_rotate(t, gray->parent);
+            gray = t->root; // 이게 좀 참신한데 맨 아래줄에서 영향을 받지 않기 위해 root로 초기화 그냥 리턴해도 되긴 할듯
+        }
+        // 좌자식 끝 우자식 시작
+        else
+        {
+            node_t* bro = gray->parent->left;
+            // case 2-2-1 : 스왑회전을 통해 문제를 남겨둔체 2-2-2로 연결
+            if (bro->color == RBTREE_RED)
+            {
+                // bro가 red면 gray -> parent 는 black
+                bro->color = RBTREE_BLACK;
+                gray->parent->color = RBTREE_RED;
+                right_rotate(t, gray->parent);
+                bro = gray->parent->left;
+            }
+            // case 2-2-2 : 다음세대로 문제를 넘길지 말지 정함 진입 시 이전 세대로 문제(gray) 옮긴다
+            // 진입 조건 : bro, bro의 자식들이 모두 black인 경우 실행
+            // bro의 색을 red로 바꿔 bro의 부분트리의 bh를 1 낮추고 결국 gray -> parent의 부분트리의 bh를 1낮춘다
+            // gray의 개념을 살려 gray -> parent에게 black을 하나 위에 올린다는 의미를 담아 gray=gray ->parent 해준다
+            // 루프 다시 돌린다.
+            if (bro->left->color == RBTREE_BLACK && bro->right->color == RBTREE_BLACK)
+            {
+                bro->color = RBTREE_RED;
+                gray = gray->parent;
+                continue;
+            }
+            // case 2-2-3 : 2-2-4를 위한 밑간 작업 핵심은 bro->right를 red로 만드는 것이다(2-2-4진입조건)
+            // bro->right이 black이라면 위의 거름막에 의해 bro -> left == red;
+            // 사용 원리는 bro의 스왑회전으로 red인 자식을 올려주는 것이다
+            // 여기까지 왔다면 while문 더 이상 안돈다
+            if (bro->left->color == RBTREE_BLACK)
+            {
+                bro->color = RBTREE_RED;
+                bro->right->color = RBTREE_BLACK;
+                left_rotate(t, bro);
+                bro = gray->parent->left;
+            }
+            // case 2-2-4 : 종료
+            // 조건 : gray -> color == black, bro->color == black, bro->right->color == red
+            // 원리 : 회전을 이해하는 것이 중요하다
+            // gray->parent를 기준으로 left_rotate한다 => gray문제 해결, bro->left 변화 없음, bro->right는 black이 올려짐(이론상 gray)
+            // bro -> right가 case 1의 gray가 된다 따라서 bro->right의 색을 black으로 바꿔준다.
+            bro->color = gray->parent->color;
+            gray->parent->color = RBTREE_BLACK;
+            bro->left->color = RBTREE_BLACK;
+            right_rotate(t, gray->parent);
             gray = t->root; // 이게 좀 참신한데 맨 아래줄에서 영향을 받지 않기 위해 root로 초기화 그냥 리턴해도 되긴 할듯
         }
     }
@@ -300,17 +346,10 @@ int rbtree_erase(rbtree* T, node_t* deleting)
     node_t* damaged;
     node_t* color_loser = deleting;
     color_t origin_color = color_loser->color;
-    //편자녀 case 분리
-    if (deleting->right == T->nil && deleting->left == T->nil)
-    {
-        if (deleting->parent->right == deleting)
-            deleting->parent->right = T->nil;
-        else deleting->parent->left = T->nil;
-        origin_color = RBTREE_RED;
-    }
-    else if (deleting->left == T->nil)
-    {
 
+    //편자녀 case 분리
+    if (deleting->left == T->nil)
+    {
         damaged = deleting->right;
         RB_transplant(T, deleting, damaged);
     }
@@ -326,10 +365,13 @@ int rbtree_erase(rbtree* T, node_t* deleting)
         color_loser = subtree_min(T,deleting->right);
         origin_color = color_loser->color;
         damaged = color_loser->right;
-
 		// color_loser의 부모가 deleting이 아닐때 작업구간
-		// color_loser의 자식 damaged 위치 변경,color_loser의 우자식 설정
-        if (color_loser->parent != deleting)
+        // color_loser의 자식 damaged 위치 변경,color_loser의 우자식 설정
+        if (color_loser->parent == deleting)
+        {
+            damaged->parent = color_loser;
+        }
+        else
         {
             // x==y->right
             // damaged를 color_loser의 부모와 연결한다. 
@@ -344,7 +386,7 @@ int rbtree_erase(rbtree* T, node_t* deleting)
 		// color_loser의 부모가 deleting이 아닐때 작업구간 끝
 
         // y(color_loser) deleting의 부모와 연결
-        RB_transplant(T, deleting->parent, color_loser);
+        RB_transplant(T, deleting, color_loser);
         // y(color_loser) deleting의 왼자식과 연결
         color_loser->left = deleting->left;
         (deleting->left)->parent = color_loser;
@@ -353,13 +395,27 @@ int rbtree_erase(rbtree* T, node_t* deleting)
     }
     if (origin_color == RBTREE_BLACK)
         del_fixup(T, damaged);
-    color_t ans = deleting->key;
+    key_t ans = deleting->key;
     free(deleting);
     return ans;
 }
+static void dfs(key_t* arr, int* idx, const rbtree* t, node_t*now)
+{
+    if (now->left != t->nil)
+        dfs(arr, idx, t, now->left);
+    arr[*idx] = now->key;
+    *idx += 1;
+    if (now->right != t->nil)
+        dfs(arr, idx, t, now->right);
+}
 
-int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
+int rbtree_to_array(const rbtree* t, key_t* arr, const size_t n)
+{
     // TODO: implement to_array
+    int* idx;
+    int a = 0;
+    idx = &a;
+    dfs(arr, idx, t, t->root);
     return 0;
 }
 
